93d907e98d27e058cb50db10d827cb14d287fc3b
DedSkinhead <117524519+DedSkinhead@users.noreply.github.com>
2022-11-05 19:17:13 +0400

    C#

---
+++ daha/pr25/pr25/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace pr25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // заменяет все группы стоящих рядом точек на многоточие.
+            Console.WriteLine("Задание 1");
+            Console.WriteLine("Введите строку: ");
+            string input = Console.ReadLine();
+
+            Regex regex = new Regex(@"\.{4,}");
+            string result = regex.Replace(input, @"...");
+            Console.WriteLine(result);
+            //Вывести на экран все слова-палиндромы, содержащиеся в сообщении.
+            Console.WriteLine("Задание 2");
+
+            Console.WriteLine("Введите текст.");
+            string text = Console.ReadLine();
+            char[] separators = { ' ', ',', ':', ';', '.', '!', '?' };
+            string[] splitedText = text.Split(separators);
+
+            foreach (string str in splitedText)
+            {
+                if (str != null)
+                {
+                    if (isPolyndrom(str))
+                    {
+                        Console.WriteLine(str);
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+        private static bool isPolyndrom(string input)
+        {
+            if (input.Length > 1)
+            {
+                char[] temp = input.ToCharArray();
+                Array.Reverse(temp);
+                if (input == new string(temp))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ daha/r23/r23/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace r23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите m");
+            int m = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите n");
+            int n = int.Parse(Console.ReadLine());
+            int[,] A = new int[m, n];
+            Random rand = new Random();
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    A[i, j] = rand.Next(-1,6) ;
+                    Console.Write("{0,3}", A[i, j]);
+                }
+                Console.WriteLine();
+            }
+            
+            for (int i = 0; i < n; i++)
+            {
+                int temp;
+                int indexFirst = -1;
+                int indexLast = -1;
+                for (int k = i; k < m; k++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        if (A[k,j]==0 && indexFirst == -1)
+                        {
+                            indexFirst = k;
+                        }
+                        if (A[k, j] == 0 && indexFirst != -1)
+                        {
+                            indexLast = k;
+                           
+                        }
+                    }
+                }
+                if (indexFirst != -1 && indexLast != -1)
+                {
+                    Console.WriteLine("indexFirst: " + indexFirst + "indexLast: " + indexLast);
+                    for (int j = 0; j < n; j++)
+                    {
+                        temp = A[indexFirst, j];
+                        A[indexFirst, j] = A[indexLast, j];
+                        A[indexLast, j] = temp;
+                    }
+                }
+                Console.WriteLine("_______________________________");
+                break;
+            }
+            Console.WriteLine();
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write("{0,3}", A[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
