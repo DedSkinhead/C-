e73699bd48c8e090e6d27295f3bc9c4bc689f679
DedSkinhead <117524519+DedSkinhead@users.noreply.github.com>
2022-11-05 19:55:04 +0400

    C#

---
+++ я/pr 23/pr 23/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr_23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите m");
+            int m = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите n");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите k1");
+            int k1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите k2");
+            int k2 = int.Parse(Console.ReadLine());
+            int[,] A = new int[m, n];
+            Random rand = new Random();
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    A[i, j] = rand.Next(0, 20);
+                    Console.Write("{0,3}", A[i, j]);
+                }
+                Console.WriteLine();
+            }
+            for (int i = 0; i < m; i++)
+            {
+                A[i, k1] = A[i, k1] + A[i, k2];
+            }
+            for (int j = 0; j < m; j++)
+            {
+                int x = A[j, 1];
+                A[j, 1] = A[j, n-2];
+                A[j, n-2] = x;
+            }
+            Console.WriteLine();
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write("{0,3}", A[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ я/pr25/pr25/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace pr25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //удваивает каждое вхождение заданного символа x.
+            Console.WriteLine("Задание 1");
+            Console.WriteLine("Введите символ");
+            string x = Console.ReadLine();
+            Console.WriteLine("Введите строку");
+            string s = Console.ReadLine();
+            string newS = s.Replace(x, x + x);
+            Console.WriteLine(newS);
+            // Вывести только те слова сообщения, которые заканчиваются прописной буквой.
+            Console.WriteLine("Задание 2");
+            var str = Console.ReadLine();
+            foreach (Match match in Regex.Matches(str, @"[а-яёА-ЯЁ]*[А-ЯЁ]"))
+            {
+                Console.WriteLine(match.Value);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
